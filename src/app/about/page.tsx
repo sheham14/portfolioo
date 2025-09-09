f4835838f1be633c2a618c289c97ba169c5317ae
sheham14 <sheham.shahid@gmail.com>
2025-09-09 19:44:36 -0230

    Removed redundant imports.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from 'next/link';
-import Navbar from '../../components/Navbar';
+
 import { motion } from 'framer-motion';
 
 export default function About() {
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,6 @@
 // pages/projects.tsx
 "use client"
-import Navbar from '../../components/Navbar';
+
 
 export default function Projects() {
   return (
